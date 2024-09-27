ed9194e4304757e1f01614eed61cf0452cf2a34f
Muntasir Hassan <muntasirhassan17@gmail.com>
2024-09-27 14:56:07 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const passwordBox = document.getElementById("password");
+const length = 8;
+const possibleChar =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*(){}-=+_<>/.";
+const copyBtn = document.getElementById("copy");
+
+function createPassword() {
+  let password = "";
+
+  while (length > password.length) {
+    password += possibleChar[Math.floor(Math.random() * possibleChar.length)];
+  }
+  passwordBox.value = password;
+}
+
+copyBtn.addEventListener("click", () => {
+  passwordBox.select();
+  document.execCommand("copy");
+  passwordBox.blur();
+});
